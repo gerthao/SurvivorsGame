04f8bab65b6310450c069cf6be656f89fadc9d02
Ger Thao <g.thao19@gmail.com>
2023-04-29 03:23:54 -0500

    add styling for victory screen, add styling for buttons.

--- scenes/ui/end_screen/end_screen.gd
+++ scenes/ui/end_screen/end_screen.gd
@@ -1,10 +1,23 @@
 extends CanvasLayer
 
+@onready var panel_container = $%PanelContainer
 
 func _ready():
+	initialize_panel()
 	get_tree().paused = true
 	$%RestartButton.pressed.connect(on_restart_pressed)
 	$%QuitButton.pressed.connect(on_quit_pressed)
+
+
+func initialize_panel() -> void:
+	panel_container.pivot_offset = panel_container.size / 2
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3) \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_BACK)
+
 
 
 func set_defeat() -> void:
